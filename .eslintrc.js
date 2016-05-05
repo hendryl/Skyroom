830b644bcc12013b3cdd9df863259622b3a7d51a
Hendry Lee <email.hendry.lee@gmail.com>
2016-05-06 00:19:02 +0700

    Remove annoying eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
   // add your custom rules here
   'rules': {
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': 0,
+    "eqeqeq": ["error", "smart"]
   }
 }
