04039d7447e4c41ea45e52ff992b451e569bb730
Hendry Lee <email.hendry.lee@gmail.com>
2016-05-21 23:44:33 +0700

    Create star destination chooser content

--- src/components/star/index.vue
+++ src/components/star/index.vue
@@ -2,8 +2,54 @@
   <div id="star" class="page">
     <div class="container">
       <div class="row">
-        <div class="col-xs-12">
-          <center>This page is under construction!</center>
+        <div class="col-xs-12 trapesium">
+          <img class="img-responsive center-block"
+          src="../../assets/images/star/start/trap_1.png"
+          alt="" />
+        </div>
+      </div>
+      <div class="row destinations">
+        <div class="anchor" id="des">
+          <a href="">
+            <img class="img-responsive"
+            src="../../assets/images/star/start/des.png"
+            alt="" />
+          </a>
+        </div>
+        <div class="anchor" id="com">
+          <a href="">
+            <img class="img-responsive"
+            src="../../assets/images/star/start/com.png"
+            alt="" />
+          </a>
+        </div>
+        <div class="anchor" id="mas">
+          <a href="">
+            <img class="img-responsive"
+            src="../../assets/images/star/start/mas.png"
+            alt="" />
+          </a>
+        </div>
+        <div class="anchor" id="temp">
+          <a href="">
+            <img class="img-responsive"
+            src="../../assets/images/star/start/temp.png"
+            alt="" />
+          </a>
+        </div>
+        <div class="anchor" id="death">
+          <a href="">
+            <img class="img-responsive"
+            src="../../assets/images/star/start/death.png"
+            alt="" />
+          </a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 trapesium">
+          <img class="img-responsive center-block"
+          src="../../assets/images/star/start/trap_2.png"
+          alt="" />
         </div>
       </div>
     </div>
@@ -21,8 +67,63 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../../assets/css/variables.scss";
 
 #star {
   background-image: url('../../assets/images/home/home_bg.png');
 }
+
+.destinations {
+  display: flex;
+  justify-content: center;
+  margin: 20px 0;
+}
+
+.anchor {
+  margin-left: -2px;
+
+  &:first-of-type {
+    margin-left: 0px;
+  }
+
+  &:hover {
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    img {
+      opacity: 0;
+    }
+  }
+}
+
+#des:hover {
+  background-image: url('../../assets/images/star/start/des_hover.png');
+}
+
+#com:hover {
+  background-image: url('../../assets/images/star/start/com_hover.png');
+}
+
+#mas:hover {
+  background-image: url('../../assets/images/star/start/mas_hover.png');
+}
+
+#temp:hover {
+  background-image: url('../../assets/images/star/start/temp_hover.png');
+}
+
+#death:hover {
+  background-image: url('../../assets/images/star/start/death_hover.png');
+}
+
+
+.arm-wrapper {
+  margin-top: 20px;
+}
+
+.arm {
+  position: absolute;
+  right: 0;
+}
+
 </style>
--- src/route-config.js
+++ src/route-config.js
@@ -6,6 +6,21 @@ export function configRouter(router) {
     '/star': {
       component: require('./components/star/index.vue'),
     },
+    '/star/description': {
+      component: require('./components/star/index.vue'),
+    },
+    '/star/composition': {
+      component: require('./components/star/index.vue'),
+    },
+    '/star/mass': {
+      component: require('./components/star/index.vue'),
+    },
+    '/star/temperature': {
+      component: require('./components/star/index.vue'),
+    },
+    '/star/death': {
+      component: require('./components/star/index.vue'),
+    },
     '/constellation': {
       component: require('./components/constellation/index.vue'),
     },
