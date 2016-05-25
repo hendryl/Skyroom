7e2669d1810f6b748f628b75f0f8b096371f2727
Hendry Lee <email.hendry.lee@gmail.com>
2016-05-25 22:43:12 +0700

    Create hover style

--- src/components/constellation/skymap.vue
+++ src/components/constellation/skymap.vue
@@ -33,62 +33,74 @@
         <div class="zodiacs">
           <div class="col-xs-4 col-sm-3">
             <a>
-              <img class="img-responsive center-block" src="../../assets/images/constellation/skymap/zodiacs/capricornnus.png" alt="capricornnus">
+              <img class="img-responsive center-block hover-hidden" src="../../assets/images/constellation/skymap/zodiacs/capricornnus.png" alt="capricornnus">
+              <img class="img-responsive center-block hover-visible" src="../../assets/images/constellation/skymap/zodiacs/capricornnus_hover.png" alt="capricornnus">
             </a>
           </div>
           <div class="col-xs-4 col-sm-3">
             <a>
-              <img class="img-responsive center-block" src="../../assets/images/constellation/skymap/zodiacs/aquarius.png" alt="aquarius">
+              <img class="img-responsive center-block hover-hidden" src="../../assets/images/constellation/skymap/zodiacs/aquarius.png" alt="aquarius">
+              <img class="img-responsive center-block hover-visible" src="../../assets/images/constellation/skymap/zodiacs/aquarius_hover.png" alt="aquarius">
             </a>
           </div>
           <div class="col-xs-4 col-sm-3">
             <a>
-              <img class="img-responsive center-block" src="../../assets/images/constellation/skymap/zodiacs/taurus.png" alt="taurus">
+              <img class="img-responsive center-block hover-hidden" src="../../assets/images/constellation/skymap/zodiacs/taurus.png" alt="taurus">
+              <img class="img-responsive center-block hover-visible" src="../../assets/images/constellation/skymap/zodiacs/taurus_hover.png" alt="taurus">
             </a>
           </div>
           <div class="col-xs-4 col-sm-3">
             <a>
-              <img class="img-responsive center-block" src="../../assets/images/constellation/skymap/zodiacs/gemini.png" alt="gemini">
+              <img class="img-responsive center-block hover-hidden" src="../../assets/images/constellation/skymap/zodiacs/gemini.png" alt="gemini">
+              <img class="img-responsive center-block hover-visible" src="../../assets/images/constellation/skymap/zodiacs/gemini_hover.png" alt="gemini">
             </a>
           </div>
           <div class="col-xs-4 col-sm-3">
             <a>
-              <img class="img-responsive center-block" src="../../assets/images/constellation/skymap/zodiacs/virgo.png" alt="virgo">
+              <img class="img-responsive center-block hover-hidden" src="../../assets/images/constellation/skymap/zodiacs/virgo.png" alt="virgo">
+              <img class="img-responsive center-block hover-visible" src="../../assets/images/constellation/skymap/zodiacs/virgo_hover.png" alt="virgo">
             </a>
           </div>
           <div class="col-xs-4 col-sm-3">
             <a>
-              <img class="img-responsive center-block" src="../../assets/images/constellation/skymap/zodiacs/libra.png" alt="libra">
+              <img class="img-responsive center-block hover-hidden" src="../../assets/images/constellation/skymap/zodiacs/libra.png" alt="libra">
+              <img class="img-responsive center-block hover-visible" src="../../assets/images/constellation/skymap/zodiacs/libra_hover.png" alt="libra">
             </a>
           </div>
           <div class="col-xs-4 col-sm-3">
             <a>
-              <img class="img-responsive center-block" src="../../assets/images/constellation/skymap/zodiacs/pisces.png" alt="pisces">
+              <img class="img-responsive center-block hover-hidden" src="../../assets/images/constellation/skymap/zodiacs/pisces.png" alt="pisces">
+              <img class="img-responsive center-block hover-visible" src="../../assets/images/constellation/skymap/zodiacs/pisces_hover.png" alt="pisces">
             </a>
           </div>
           <div class="col-xs-4 col-sm-3">
             <a>
-              <img class="img-responsive center-block" src="../../assets/images/constellation/skymap/zodiacs/aries.png" alt="aries">
+              <img class="img-responsive center-block hover-hidden" src="../../assets/images/constellation/skymap/zodiacs/aries.png" alt="aries">
+              <img class="img-responsive center-block hover-visible" src="../../assets/images/constellation/skymap/zodiacs/aries_hover.png" alt="aries">
             </a>
           </div>
           <div class="col-xs-4 col-sm-3">
             <a>
-              <img class="img-responsive center-block" src="../../assets/images/constellation/skymap/zodiacs/cancer.png" alt="cancer">
+              <img class="img-responsive center-block hover-hidden" src="../../assets/images/constellation/skymap/zodiacs/cancer.png" alt="cancer">
+              <img class="img-responsive center-block hover-visible" src="../../assets/images/constellation/skymap/zodiacs/cancer_hover.png" alt="cancer">
             </a>
           </div>
           <div class="col-xs-4 col-sm-3">
             <a>
-              <img class="img-responsive center-block" src="../../assets/images/constellation/skymap/zodiacs/leo.png" alt="leo">
+              <img class="img-responsive center-block hover-hidden" src="../../assets/images/constellation/skymap/zodiacs/leo.png" alt="leo">
+              <img class="img-responsive center-block hover-visible" src="../../assets/images/constellation/skymap/zodiacs/leo_hover.png" alt="leo">
             </a>
           </div>
           <div class="col-xs-4 col-sm-3">
             <a>
-              <img class="img-responsive center-block" src="../../assets/images/constellation/skymap/zodiacs/scorpionus.png" alt="scorpionus">
+              <img class="img-responsive center-block hover-hidden" src="../../assets/images/constellation/skymap/zodiacs/scorpius.png" alt="scorpius">
+              <img class="img-responsive center-block hover-visible" src="../../assets/images/constellation/skymap/zodiacs/scorpius_hover.png" alt="scorpionus">
             </a>
           </div>
           <div class="col-xs-4 col-sm-3">
-            <a>
-              <img class="img-responsive center-block" src="../../assets/images/constellation/skymap/zodiacs/sagitarius.png" alt="sagitarius">
+            <a data-toggle="modal" data-target="#myModal">
+              <img class="img-responsive center-block hover-hidden" src="../../assets/images/constellation/skymap/zodiacs/sagitarius.png" alt="sagitarius">
+              <img class="img-responsive center-block hover-visible" src="../../assets/images/constellation/skymap/zodiacs/sagitarius_hover.png" alt="sagitarius">
             </a>
           </div>
         </div>
@@ -140,8 +152,24 @@ export default {};
 a {
   display: block;
 
+  .hover-visible {
+    display: none;
+  }
+
+  .hover-hidden {
+    display: block;
+  }
+
   &:hover {
     cursor: pointer;
+
+    .hover-visible {
+      display: block;
+    }
+
+    .hover-hidden {
+      display: none;
+    }
   }
 }
 </style>
