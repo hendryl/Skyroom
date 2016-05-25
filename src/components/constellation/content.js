b2635f2b90700e8ef7b07fc56e4759c056ff729f
Hendry Lee <email.hendry.lee@gmail.com>
2016-05-26 00:31:09 +0700

    Add all needed content

--- src/components/constellation/content.js
+++ src/components/constellation/content.js
@@ -2,50 +2,50 @@
 export default {
   aries: {
     date: '21 March - 20 April',
-    text: 'Aries was the pet ram of Zeus that had a golden fleece instead of white wool. One day, Aries was sent to help two children who were in danger of being killed. Aries arrived just in time for the children to jump on his back. He then raced to safety. To honor the effor his ram had made, Zeus placed him in the heavens where he can roam freely near the flying horse Pegasus.',
+    text: 'Aries was the pet ram of Zeus that had a golden fleece instead of white wool. One day, Aries was sent to help two children who were in danger of being killed. Aries arrived just in time for the children to jump on his back. He then raced to safety. To honor the efforts his ram had made, Zeus placed him in the heavens where he could roam freely near the flying horse Pegasus.',
   },
   taurus: {
     date: '21 April - 21 May',
-    text: '',
+    text: "Zeus turned himself into a large white bull and kidnapped Europa. He took her to the island of Crete where they spent time living together. Europa gave birth to several children with Zeus, including King Minos. Taurus is the symbol that commemorates the adventure of Europa's abduction.",
   },
   gemini: {
     date: '22 May - 21 June',
-    text: '',
+    text: 'Castor and Pollux was the sons of Zeus and were very close friends. They both fought the pirates that attacked the honest seamen. The seamen carved their image on their ships to protect them from bad weather and danger. During one of the fights, Castor was killed. Pollux was filled with grief and begged Zeus to allow him to always be with Castor. Zeus was touched by his feelings of true friendship and placed them both on the sky as a sign of their friendship.',
   },
   cancer: {
     date: '22 June - 22 July',
-    text: '',
+    text: 'Cancer was the crab that was sent by Hera and pinched Hercules on the foot while he was fighting the Hydra. The crab was then stepped by Hercules and died. Hera pitied the dead crab and placed it in the sky.',
   },
   leo: {
     date: '23 July - 22 August',
-    text: '',
+    text: 'Leo was a lion that lived in a cave in the Nemean Mountains. It killed a lot of people and cattles down in the valley. In order to save the people from the Nemean Lion, Hercules was tasked to look for the lion and defeat it. He had a fierce battle with the Nemean Lion and strangled the lion until it is out of breath . He wore the skin of the Nemean Lion as a trophy of his battle.',
   },
   virgo: {
     date: '23 August - 23 September',
-    text: '',
+    text: "Proserpine was the daughter of Hera, the goddess of fertility. One day while she was out playing and picking some flowers, Hades appeared before her and kidnapped her to the underworld. Knowing that her daughter is being kidnapped, Hera cried and the plants could not grow, leaving the people starving. Zeus asked Hades to return Proserpine to stop the catastrophe. Hades agreed to Zeus' request and placed Proserpine in the sky every two seasons which is Spring and Summer.",
   },
   libra: {
     date: '24 September - 23 October',
-    text: '',
+    text: 'Libra symbolizes Astraea, the goddess of justice. She would weigh the souls of men and women on a balancing scale and hold them responsible for their acts. The Sumerians in 200 BCE called it the ‘Balance of Heaven’.',
   },
   scorpius: {
     date: '24 October - 22 November',
-    text: '',
+    text: 'Scorpius was the scorpion that was sent by Hera to battle Orion. Orion was a great hunter, but he very arrogant and killed many animals. With a poisonous sting, Scorpius stung him to death. Until now when the Orion constellation is in the east, the Scorpius constellation will rise in the west and when Scorpius is in the east, Orion will hide in the west.',
   },
   sagittarius: {
     date: '23 November - 21 December',
-    text: '',
+    text: 'Chiron was a strong and wise centaur who was the teacher of Hercules. One day, Hercules drank all the liquor in the centaur’s village and got drunk. He went berserk and starts attacking the other centaurs while he was drunk. Chiron tried to stop him but Hercules shot him with an arrow coated by the Hydra’s venom. Zeus pitied Chiron and placed him in the sky to honor him.',
   },
   capricornus: {
     date: '22 December - 20 January',
-    text: '',
+    text: 'Pricus was a sea goat that had great intelligence and was very wise. One day his children of sea goats went to the shore and became normal goats, losing their intelligence in the process. Pricus reversed time to prevent his children from becoming normal goats but in the end, they still became normal goats. Realizing that he couldn’t change fate, he drowned himself in loneliness and became the constellation of Capricornus.',
   },
   aquarius: {
     date: '21 January - 18 February',
-    text: '',
+    text: 'Ganymede was a young prince from the kingdom of Troy. Because of his beautiful face, Zeus kidnapped him and took him to Mount Olympus. There, Zeus made Ganymede his personal cupbearer for the gods. One day, Ganymede had enough working for the gods and the water from his cup starts falling to the earth, causing a great flood on earth. Realizing his horrible acts towards Ganymede, Zeus placed Ganymede in the sky to honor him.',
   },
   pisces: {
     date: '19 February - 20 March',
-    text: '',
+    text: 'Typhon was a monster who was in love with the beautiful goddess Aphrodite. One day, he pursued her while she and her son Eros were walking along the bank of the Euphrates River. Some river nymphs protected them by transforming them into fishes. To show eternal appreciation, fishes were placed in the sky.',
   },
 };
