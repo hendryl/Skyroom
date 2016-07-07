9497c6a5a725461e4f1766e81c47686c980f67af
Hendry Lee <email.hendry.lee@gmail.com>
2016-07-07 21:53:46 +0700

    Force transition to start at top of page

--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import { configRouter } from './route-config';
 
-import App from './App';
+import App from './App.vue';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter();
 
 configRouter(router);
+
+router.beforeEach((transition) => {
+  window.scrollTo(0, 0);
+  transition.next();
+});
+
 router.start(App, '#app');
 
 window.router = router;
